8e2f6f8fd781205a519dfbab1d666f11a0170dce
HiiFyi <167199917+HiiFyi@users.noreply.github.com>
2024-11-16 17:29:06 +0530

    Update gfilters_mdb.py

--- database/gfilters_mdb.py
+++ database/gfilters_mdb.py
@@ -4,7 +4,7 @@
 
 
 import pymongo
-from info import DATABASE_URI, DATABASE_NAME, SECONDDB_URI
+from info import DATABASE_URI, DATABASE_NAME, SECONDDB_URI, THIRDDB_URL
 from pyrogram import enums
 from sample_info import tempDict
 import logging
@@ -14,6 +14,9 @@ logger.setLevel(logging.ERROR)
 myclient = pymongo.MongoClient(DATABASE_URI)
 mydb = myclient[DATABASE_NAME]
 
+myclient3 = pymongo.MongoClient(THIRDDB_URL)
+mydb3 = myclient3[DATABASE_NAME]
+
 myclient2 = pymongo.MongoClient(SECONDDB_URI)
 mydb2 = myclient2[DATABASE_NAME]
 
@@ -21,6 +24,8 @@ async def add_gfilter(gfilters, text, reply_text, btn, file, alert):
     #check indexDB
     if tempDict['indexDB'] == DATABASE_URI:
         mycol = mydb[str(gfilters)]
+    else:
+        mycol = mydb3[str(gfilters)]
     else:
         mycol = mydb2[str(gfilters)]
     # mycol.create_index([('text', 'text')])
@@ -41,10 +46,26 @@ async def add_gfilter(gfilters, text, reply_text, btn, file, alert):
      
 async def find_gfilter(gfilters, name):
     mycol = mydb[str(gfilters)]
+    mycol3 = mydb3[str(gfilters)]
     mycol2 = mydb2[str(gfilters)]
     
     query = mycol.find( {"text":name})
     # query = mycol.find( { "$text": {"$search": name}})
+    if query.count() == 0:
+        query = mycol3.find({"text": name})
+    try:
+        for file in query:
+            reply_text = file['reply']
+            btn = file['btn']
+            fileid = file['file']
+            try:
+                alert = file['alert']
+            except:
+                alert = None
+        return reply_text, btn, alert, fileid
+    except:
+        return None, None, None, None
+    
     if query.count() == 0:
         query = mycol2.find({"text": name})
     try:
@@ -63,13 +84,21 @@ async def find_gfilter(gfilters, name):
 
 async def get_gfilters(gfilters):
     mycol = mydb[str(gfilters)]
+    mycol3 = mydb3[str(gfilters)]
     mycol2 = mydb2[str(gfilters)]
 
     texts = []
     query = mycol.find()
+    query3 = mycol3.find()
     query2 = mycol2.find()
     try:
         for file in query:
+            text = file['text']
+            texts.append(text)
+    except:
+        pass
+    try:
+        for file in query3:
             text = file['text']
             texts.append(text)
     except:
@@ -85,10 +114,12 @@ async def get_gfilters(gfilters):
 
 async def delete_gfilter(message, text, gfilters):
     mycol = mydb[str(gfilters)]
+    mycol3 = mydb3[str(gfilters)]
     mycol2 = mydb2[str(gfilters)]
 
     myquery = {'text':text }
     query = mycol.count_documents(myquery)
+    query3 = mycol3.count_documents(myquery)
     query2 = mycol2.count_documents(myquery)
     if query == 1:
         mycol.delete_one(myquery)
@@ -97,6 +128,14 @@ async def delete_gfilter(message, text, gfilters):
             quote=True,
             parse_mode=enums.ParseMode.MARKDOWN
         )
+    elif:
+        if query3 == 1:
+            mycol3.delete_one(myquery)
+            await message.reply_text(
+                f"'`{text}`'  deleted. I'll not respond to that gfilter anymore.",
+                quote=True,
+                parse_mode=enums.ParseMode.MARKDOWN
+            )
     else:
         if query2 == 1:
             mycol2.delete_one(myquery)
@@ -109,14 +148,16 @@ async def delete_gfilter(message, text, gfilters):
             await message.reply_text("Couldn't find that gfilter!", quote=True)
 
 async def del_allg(message, gfilters):
-    if str(gfilters) not in mydb.list_collection_names() and str(gfilters) not in mydb2.list_collection_names():
+    if str(gfilters) not in mydb.list_collection_names() and str(gfilters) not in mydb3.list_collection_names() not in mydb2.list_collection_names():
         await message.edit_text("Nothing to Remove !")
         return
 
     mycol = mydb[str(gfilters)]
+    mycol3 = mydb3[str(gfilters)]
     mycol2 = mydb2[str(gfilters)]
     try:
         mycol.drop()
+        mycol3.drop()
         mycol2.drop()
         await message.edit_text(f"All gfilters has been removed !")
     except:
@@ -125,19 +166,23 @@ async def del_allg(message, gfilters):
 
 async def count_gfilters(gfilters):
     mycol = mydb[str(gfilters)]
+    mycol3 = myd3[str(gfilters)]
     mycol2 = mydb2[str(gfilters)]
 
     count = mycol.count()
-    count = ((mycol.count())+(mycol2.count()))
+    count = ((mycol.count())+(mycol3.count())+(mycol2.count()))
     return False if count == 0 else count
 
 
 async def gfilter_stats():
     collections = mydb.list_collection_names()
+    collections3 = mydb3.list_collection_names()
     collections2 = mydb2.list_collection_names()
 
     if "CONNECTION" in collections:
         collections.remove("CONNECTION")
+    elif "CONNECTION" in collections3:
+        collections3.remove("CONNECTION")
     elif "CONNECTION" in collections2:
         collections2.remove("CONNECTION")
 
@@ -147,11 +192,16 @@ async def gfilter_stats():
         count = mycol.count()
         totalcount += count
 
+    for collection in collections3:
+        mycol3 = mydb3[collection]
+        count3 = mycol3.count()
+        totalcount += count3
+
     for collection in collections2:
         mycol2 = mydb2[collection]
         count2 = mycol2.count()
         totalcount += count2
 
-    totalcollections = len(collections)+len(collections2)
+    totalcollections = len(collections)+len(collections3)+len(collections2)
 
     return totalcollections, totalcount
